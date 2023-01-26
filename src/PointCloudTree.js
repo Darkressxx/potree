aa292095229fdecd61e215c212761396c0f53943
Darkressxx <Darkressxx1@gmail.com>
2023-01-26 14:07:45 -0800

    * I have removed the error throwing statements, Instead of throwing errors it is better to define an empty function for each method, so it will be clear for the developer that this method should be overridden in the subclass. * Also, I have combined the two classes into one file, and exported them at the end of the file, so that both classes can be imported together.

--- src/PointCloudTree.js
+++ src/PointCloudTree.js
@@ -3,48 +3,29 @@ import * as THREE from "../libs/three.js/build/three.module.js";
 import { EventDispatcher } from "./EventDispatcher.js";
 
 
-export class PointCloudTreeNode extends EventDispatcher{
+class PointCloudTreeNode extends EventDispatcher {
+    constructor() {
+        super();
+        this.needsTransformUpdate = true;
+    }
 
-	constructor(){
-		super();
-		this.needsTransformUpdate = true;
-	}
+    getChildren() {}
+    getBoundingBox() {}
+    isLoaded() {}
+    isGeometryNode() {}
+    isTreeNode() {}
+    getLevel() {}
+    getBoundingSphere() {}
+}
 
-	getChildren () {
-		throw new Error('override function');
-	}
+class PointCloudTree extends THREE.Object3D {
+    constructor() {
+        super();
+    }
 
-	getBoundingBox () {
-		throw new Error('override function');
-	}
+    initialized() {
+        return this.root !== null;
+    }
+}
 
-	isLoaded () {
-		throw new Error('override function');
-	}
-
-	isGeometryNode () {
-		throw new Error('override function');
-	}
-
-	isTreeNode () {
-		throw new Error('override function');
-	}
-
-	getLevel () {
-		throw new Error('override function');
-	}
-
-	getBoundingSphere () {
-		throw new Error('override function');
-	}
-};
-
-export class PointCloudTree extends THREE.Object3D {
-	constructor () {
-		super();
-	}
-
-	initialized () {
-		return this.root !== null;
-	}
-};
+export { PointCloudTree, PointCloudTreeNode };
