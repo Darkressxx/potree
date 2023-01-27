241a0f7f659e520bfdc71236fdc3efcfc674b6ca
Darkressxx <Darkressxx1@gmail.com>
2023-01-27 06:38:24 -0800

    Update WorkerPool.js
    * Move the creation of the worker object out of the getWorker function so that it is only created once, instead of each time the function is called. Additionally, a limit can be set on the number of worker objects that can be created for a given url to prevent overloading the system with too many worker objects.

--- src/WorkerPool.js
+++ src/WorkerPool.js
@@ -1,27 +1,32 @@
+export class WorkerPool {
+  constructor() {
+	this.workers = {};
+	this.maxWorkers = 5;
+  }
 
-export class WorkerPool{
-	constructor(){
-		this.workers = {};
+  getWorker(url) {
+	if (!this.workers[url]) {
+	  this.workers[url] = [];
 	}
 
-	getWorker(url){
-		if (!this.workers[url]){
-			this.workers[url] = [];
-		}
-
-		if (this.workers[url].length === 0){
-			let worker = new Worker(url);
-			this.workers[url].push(worker);
-		}
-
-		let worker = this.workers[url].pop();
-
-		return worker;
+	let worker;
+	if (this.workers[url].length === 0) {
+	  if (this.workers[url].length < this.maxWorkers) {
+		worker = new Worker(url);
+	  } else {
+		// return null or throw an error if maximum number of workers is reached
+	  }
+	} else {
+	  worker = this.workers[url].pop();
 	}
 
-	returnWorker(url, worker){
-		this.workers[url].push(worker);
-	}
+	return worker;
+  }
+
+  returnWorker(url, worker) {
+	this.workers[url].push(worker);
+  }
 };
+
 
 //Potree.workerPool = new Potree.WorkerPool();
