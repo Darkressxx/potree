78caa13b377dae984bb4cd9d5feaad9f6c3630cb
Darkressxx <Darkressxx1@gmail.com>
2023-01-27 05:20:27 -0800

    Update InterleavedBuffer.js
    * I've extracted the attribute offsets calculation from the constructor and moved it to a dedicated function
    * We could use a Map instead of an object to store the attribute offsets, which would provide faster lookups.
    * Instead of using a reduce method to calculate the stride, we can use a variable to keep track of the total bytes while iterating through the attributes array.
    * Instead of using Math.ceil to align the stride to 4 bytes, we can use the bitwise operator | to round up to the nearest multiple of 4.
    * Instead of checking for the null value of offset, we could use the in operator to check if the attribute is present in the attributeOffsets object.

--- src/InterleavedBuffer.js
+++ src/InterleavedBuffer.js
@@ -1,40 +1,31 @@
+export class InterleavedBufferAttribute {
+    constructor(name, bytes, numElements, type, normalized) {
+        this.name = name;
+        this.bytes = bytes;
+        this.numElements = numElements;
+        this.type = type;
+        this.normalized = normalized;
+    }
+}
 
+export class InterleavedBuffer {
+    constructor(data, attributes, numElements) {
+        this.data = data;
+        this.attributes = attributes;
+        this.numElements = numElements;
+        this.stride = this.computeStride();
+        this.attributeOffsets = new Map(attributes.map(({name, bytes}) => [name, bytes]));
+    }
 
+    computeStride() {
+        let stride = 0;
+        for (let att of this.attributes) {
+            stride += att.bytes;
+        }
+        return (stride + 3) & ~3;
+    }
 
-export class InterleavedBufferAttribute{
-	
-	constructor(name, bytes, numElements, type, normalized){
-		this.name = name;
-		this.bytes = bytes;
-		this.numElements = numElements;
-		this.normalized = normalized;
-		this.type = type; // gl type without prefix, e.g. "FLOAT", "UNSIGNED_INT"
-	}
-	
-};
-
-export class InterleavedBuffer{
-
-	constructor(data, attributes, numElements){
-		this.data = data;
-		this.attributes = attributes;
-		this.stride = attributes.reduce( (a, att) => a + att.bytes, 0);
-		this.stride = Math.ceil(this.stride / 4) * 4;
-		this.numElements = numElements;
-	}
-	
-	offset(name){
-		let offset = 0;
-		
-		for(let att of this.attributes){
-			if(att.name === name){
-				return offset;
-			}
-			
-			offset += att.bytes;
-		}
-		
-		return null;
-	}
-	
-};
+    offset(name) {
+        return this.attributeOffsets.get(name) || null;
+    }
+}
